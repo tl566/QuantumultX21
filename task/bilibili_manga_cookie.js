b7d5720b846cac13735bf1bb55dcc7daa4dad822
BautistaClay <80048034+BautistaClay@users.noreply.github.com>
2023-08-21 03:59:50 +0800

    Create bilibili_manga_cookie.js

---
+++ task/bilibili_manga_cookie.js
@@ -0,0 +1,3 @@
+hostname = app.bilibili.com
+#获取Bili漫画Cookie
+^https:\/\/app\.bilibili\.com\/x\/v2\/account\/myinfo url script-request-header https://raw.githubusercontent.com/BautistaClay/QuantumultX/main/task/bilibili_manga.js
